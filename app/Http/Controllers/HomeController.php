39bdbaf83980978671e0d6cab687661037dd4f19
Gabriel Fagundes <aluzgabriel@hotmail.com>
2018-01-10 23:08:00 -0200

    Início da implementação da tela inicial do app

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Tutorial;
+use App\Youtube;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-
+        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $youtube = new Youtube();
+        $tutorials = Tutorial::all();
+        $videos = $youtube->returnVideoContent();
+
+        return view('home')->with(compact('tutorials', 'videos'));
     }
 }
